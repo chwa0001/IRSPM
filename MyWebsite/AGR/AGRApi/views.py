4d4129400513bb098dc5c0002c885af3f95618c3
antoniadevina <21048103+antoniadevina@users.noreply.github.com>
2021-03-07 22:20:30 +0800

    test

--- MyWebsite/AGR/AGRApi/views.py
+++ MyWebsite/AGR/AGRApi/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # Create your views here.
-
+ 
 
 class UserView(generics.ListAPIView):
     queryset = User.objects.all()
