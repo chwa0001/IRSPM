e022265138b16fbb085aedc2fada28f2280a1c2a
antoniadevina <21048103+antoniadevina@users.noreply.github.com>
2021-03-17 23:16:29 +0800

    change in layout. still need to add scrollbar
    scroll bar should be added in the Home page

--- MyWebsite/AGR/AGRFrontend/src/_ExData.js
+++ MyWebsite/AGR/AGRFrontend/src/_ExData.js
@@ -1,25 +1,25 @@
 const tileData = [
     {
-      img1: '/AGRFrontend/static/images/4.jpg',
-      img2: '/AGRFrontend/static/images/5.jpg',
-      exercise_name: 'Breakfast',
-      main_muscle: 'Abss',
-      detail_muscle: 'Absss',
-      other_muscle: 'Abs?',
-      type: 'Strength',
-      mechanics: 'Compound',
-      equipment: 'Body',
-      difficulty: 'Intermediate',
+      img1: '/AGRFrontend/static/images/8.jpg',
+      img2: '/AGRFrontend/static/images/9.jpg',
+      exercise_name: 'String Pull',
+      main_muscle: 'Absss',
+      detail_muscle: 'Abs',
+      other_muscle: 'Top Abs',
+      type: 'Strength 2',
+      mechanics: 'Compound 2',
+      equipment: 'Body 2',
+      difficulty: 'Intermediate 2',
       Instructions: `Steps : 
-
+      \n\n
       1.) Start by laying on your back with arms at your sides , keeping one knee bent with your heel on the ground and the other leg pointed straight out in front of you in a 45 degree angle.
-      
+      \n\n
       2.) Slowly lift your hips off of the ground and squeeze your glutes.
-      
+      \n\n
       3.) Form a straight line with your body through your shoulders and knees.
-      
+      \n\n
       4.) Hold onto this position for a few seconds then return back to the starting position.
-      
+      \n\n
       5.) Alternate legs and repeat.`,
     },
     {
@@ -71,7 +71,7 @@ const tileData = [
     {
       img1: '/AGRFrontend/static/images/8.jpg',
       img2: '/AGRFrontend/static/images/9.jpg',
-      exercise_name: 'Breakfast',
+      exercise_name: 'String Pull',
       main_muscle: 'Absss',
       detail_muscle: 'Abs',
       other_muscle: 'Top Abs',
@@ -80,15 +80,15 @@ const tileData = [
       equipment: 'Body 2',
       difficulty: 'Intermediate 2',
       Instructions: `Steps : 
-
+      \n\n
       1.) Start by laying on your back with arms at your sides , keeping one knee bent with your heel on the ground and the other leg pointed straight out in front of you in a 45 degree angle.
-      
+      \n\n
       2.) Slowly lift your hips off of the ground and squeeze your glutes.
-      
+      \n\n
       3.) Form a straight line with your body through your shoulders and knees.
-      
+      \n\n
       4.) Hold onto this position for a few seconds then return back to the starting position.
-      
+      \n\n
       5.) Alternate legs and repeat.`,
     },
     {
